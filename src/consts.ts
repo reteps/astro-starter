92efcd5c9fea8b647c75835637080edfea34eeee
Pete Stenger <pete@stenger.io>
2024-10-21 22:05:08 -0500

    add changes

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-export const SITE_URL: string = "https://astrostarter.zank.studio";
+export const SITE_URL: string = "http://localhost:4321";
+// s://astrostarter.zank.studio
 export const SITE_TITLE: string = "Astro Blog";
 export const SITE_DESCRIPTION: string = "Welcome to my website!";
 
